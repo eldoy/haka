ff406b3a38b83a925756b975a9ebb0a717a8c2a6
Vidar Eldøy <vidar@eldoy.com>
2020-10-18 06:24:26 +0200

    New build

--- dist/haka.js
+++ dist/haka.js
@@ -219,14 +219,18 @@ window.flash = function(message, opt) {
   if (!opt) opt = {}
   var el = q(opt.el || '#flash'), time = opt.time || 5000, name = opt.name || 'flash'
   if (!el) return null
-  if (typeof window.__$timeout != 'undefined') {
-    clearTimeout(window.__$timeout)
+  if (typeof window.__$flash != 'undefined') {
+    clearTimeout(window.__$flash)
   }
   message = (message || cookie(name) || '').trim()
   cookie(name, null)
   if (opt.scroll != false) scroll(0, 0)
+  if (opt.class) el.classList.add(opt.class)
   el.textContent = message
   el.style.opacity = 1
-  if (time) window.__$timeout = setTimeout(function() { el.style.opacity = 0 }, time)
+  if (time) window.__$flash = setTimeout(function() {
+    el.style.opacity = 0
+    if (opt.class) el.classList.remove(opt.class)
+  }, time)
   return el
 }
