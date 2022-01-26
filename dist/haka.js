33dc0c4b4de4470a9818c3c048bb5c555b079c53
Vidar Eldøy <vidar@eldoy.com>
2022-01-26 09:46:41 +0100

    Prevent double get

--- dist/haka.js
+++ dist/haka.js
@@ -267,7 +267,7 @@ window.serialize = function(form) {
       else if (field.type != 'radio' || field.checked) {
         var val = get(field)
         if (typeof val != 'undefined') {
-          data[field.name] = get(field)
+          data[field.name] = val
         }
       }
     }
