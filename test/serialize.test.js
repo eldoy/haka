8528e78185dca0ba17758c2feb4340fcc8395de3
Vidar Eldøy <vidar@eldoy.com>
2022-02-23 20:19:49 +0100

    Add test for empty date input

--- test/serialize.test.js
+++ test/serialize.test.js
@@ -140,8 +140,13 @@ describe('serialize', () => {
     expect(data().hello).toBe(false)
   })
 
+  it('should not get object from empty date', () => {
+    form(`<input name="hello" type="date">`)
+    expect(data().hello).toBeUndefined()
+  })
+
   it('should be a date object date type', () => {
-    form(`<input name="hello" data-type="date" value="2022-01-20">`)
+    form(`<input name="hello" type="date" value="2022-01-20">`)
     expect(data().hello instanceof Date).toBe(true)
     expect(data().hello.getUTCDate()).toBe(20)
     expect(data().hello.getUTCMonth()).toBe(0)
