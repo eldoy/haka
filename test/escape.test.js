1416cd1b1c03f345c30df78f5694da80c002aa49
Vidar Eldøy <vidar@eldoy.com>
2022-07-20 17:31:05 +0200

    Update escape.test.js

--- test/escape.test.js
+++ test/escape.test.js
@@ -14,9 +14,9 @@ describe('escape', () => {
   })
 
   it('should unescape strings', () => {
-    expect(esc(undefined)).toBe(undefined)
-    expect(esc(null)).toBe(null)
-    expect(esc('')).toBe('')
+    expect(raw(undefined)).toBe(undefined)
+    expect(raw(null)).toBe(null)
+    expect(raw('')).toBe('')
     expect(raw('hello')).toBe('hello')
     expect(raw('&lt;div&gt;hello&lt;/div&gt;')).toBe('<div>hello</div>')
   })
